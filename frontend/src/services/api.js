bb27be60f8d3f45ff23f4aad8f574a795836c770
Varun Tiwari <tiwarivarun726@gmail.com>
2026-06-18 13:29:20 +0530

    Fix production API path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://event-ticket-booking-system-8h0w.onrender.com",
+  baseURL: "https://event-ticket-booking-system-8h0w.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
